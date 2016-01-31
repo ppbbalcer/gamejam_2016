fe5927d6fe37a9be900bdb2ada2466717a9e0d68
maciej <maciej.a.kaminski@gmail.com>
2016-01-31 11:47:41 +0100

    update walls
    Signed-off-by: maciej <maciej.a.kaminski@gmail.com>

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -367,7 +367,7 @@ void Engine::mainLoop() {
 		if (_font && !status_line.empty()) {
 			SDL_RenderSetViewport(_renderer, &_viewportScreen);
 			_font->printf(screen_width() / 2,
-				      screen_height() *0.9,
+				      screen_height() -20 ,
 				      ALIGN_BOTTOM_WITH_PADDING,
 				      status_line.c_str());
 		}
