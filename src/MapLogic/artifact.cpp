7a4f79900e05d0912eef0be6c57b772d546d1bbe
Piotr Balcer <ppbbalcer@gmail.com>
2016-01-31 11:29:14 +0100

    add artifact glow

--- src/MapLogic/artifact.cpp
+++ src/MapLogic/artifact.cpp
@@ -10,10 +10,26 @@ void Artifact::SteppedOver(Character * who)
 
 void Artifact::update(int time)
 {
-
+	if (glowing) {
+		glow_tick_timer -= time;
+		if (glow_tick_timer < 0) {
+			tile++;
+			if (tile > ARTIFACT_TILE_END) {
+				tile = ARTIFACT_TILE_BEGIN;
+				glowing = 0;
+			}
+			glow_tick_timer = ARTIFACT_GLOW_TICK;
+		}
+	} else {
+		glow_delay_timer -= time;
+		if (glow_delay_timer < 0) {
+			glowing = 1;
+			glow_delay_timer = ARTIFACT_GLOW_DELAY;
+		}
+	}
 }
 
 int Artifact::GetTileId()
 {
-	return 58;
+	return tile;
 }
--- src/MapLogic/artifact.h
+++ src/MapLogic/artifact.h
@@ -3,6 +3,12 @@
 #include <list>
 #include "field.h"
 
+#define ARTIFACT_GLOW_TICK 100
+#define ARTIFACT_GLOW_DELAY 1000
+
+#define ARTIFACT_TILE_BEGIN 56
+#define ARTIFACT_TILE_END 59
+
 class Artifact: public Field
 {
 public:
@@ -10,10 +16,19 @@ public:
 
 	Artifact() : Field(ARTIFACT)
 	{
+		tile = ARTIFACT_TILE_BEGIN;
+		glow_tick_timer = 0;
+		glow_delay_timer = 0;
+		glowing = 0;
 	}
 
 	virtual void update(int time);
 	virtual int GetTileId();
+
+	int tile;
+	int glowing;
+	int glow_tick_timer;
+	int glow_delay_timer;
 };
 
 #endif
